41bcca4de53208c32e8f433672ce19d22cbefa5d
kaysee <kayseeclintone@gmail.com>
2023-06-07 21:33:06 +0300

    first commit ever

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,102 @@
+import { useState, useEffect } from 'react';
+import { FaSignInAlt } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { login, reset } from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
+
+function Login() {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
+	const { user, isLoading, isError, isSuccess, message } = useSelector(
+		(state) => state.auth
+	);
+
+	useEffect(() => {
+		if (isError) {
+			// Handle error display here
+		}
+
+		if (isSuccess || user) {
+			navigate('/');
+		}
+
+		return () => {
+			dispatch(reset());
+		};
+	}, [user, isError, isSuccess, message, navigate, dispatch]);
+
+	const onChange = (e) => {
+		const { name, value } = e.target;
+		if (name === 'email') {
+			setEmail(value);
+		} else if (name === 'password') {
+			setPassword(value);
+		}
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+
+		const userData = {
+			email,
+			password,
+		};
+
+		dispatch(login(userData));
+	};
+
+	if (isLoading) {
+		return <Spinner />;
+	}
+
+	return (
+		<>
+			<section className='heading'>
+				<h1>
+					<FaSignInAlt /> Login
+				</h1>
+				<p>Login and start setting goals</p>
+			</section>
+
+			<section className='form'>
+				<form onSubmit={onSubmit}>
+					<div className='form-group'>
+						<input
+							type='email'
+							className='form-control'
+							id='email'
+							name='email'
+							value={email}
+							placeholder='Enter your email'
+							onChange={onChange}
+						/>
+					</div>
+					<div className='form-group'>
+						<input
+							type='password'
+							className='form-control'
+							id='password'
+							name='password'
+							value={password}
+							placeholder='Enter password'
+							onChange={onChange}
+						/>
+					</div>
+
+					<div className='form-group'>
+						<button type='submit' className='btn btn-block'>
+							Submit
+						</button>
+					</div>
+				</form>
+			</section>
+		</>
+	);
+}
+
+export default Login;
